c62812734f597bd1b2daa762fd7f8cf23318d45d
Varun A P <varunlakshmananap@gmail.com>
2020-06-07 16:16:53 +0200

    PostCSS fix for pagination

--- themes/portfolique/assets/css/postcss.config.js
+++ themes/portfolique/assets/css/postcss.config.js
@@ -5,7 +5,6 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     // Specify the paths to all of the template files in your project
     content: [
         themeDir + 'layouts/**/*.html',
-        // themeDir + 'exampleSite/content/**/*.html',
         'layouts/**/*.html',
         'content/**/*.html',
     ],
